05c3b877b4ea03ce72dcf329bbd2f4eeb599274e
ThangMinhCao <thangminhcao@gmail.com>
2022-01-27 15:32:23 -0500

    Add cards and grid layout to project page

--- src/sections/Projects/Projects.js
+++ src/sections/Projects/Projects.js
@@ -1,22 +1,34 @@
+import CardItem from "../../components/CardItem/CardItem";
 import Section from "../../components/Section";
 import { MediumText, Title } from "../../components/Typography";
+import { info } from "../../data/itemInfo";
 import "./Projects.css";
 
 const Projects = () => {
   return (
     <Section className="project-container">
-      <div className="project-list"></div>
+      <div className="project-list">
+        {info.experiences.map(
+          ({ company, logo, title, date, description }, index) =>
+            <CardItem
+              key={index}
+              logo={logo}
+              title={company}
+              subtitle={title}
+              date={date}
+              description={description}
+            />
+        )}
+      </div>
 
       <div className="project-title-container">
         <div className="top-space" />
         <div className="project-title">
           <MediumText>AND</MediumText>
           <Title>
-            WHAT
+            WHAT'VE
             <br />
-            COME
-            <br />
-            ALONG
+            COME ALONG
           </Title>
         </div>
       </div>
