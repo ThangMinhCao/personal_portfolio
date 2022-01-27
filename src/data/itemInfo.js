b9a78b0eadaf44664030f3cfe988223a349d35cb
ThangMinhCao <thangminhcao@gmail.com>
2022-01-27 00:14:19 -0500

    Shorten experience descriptions

--- src/data/itemInfo.js
+++ src/data/itemInfo.js
@@ -9,42 +9,42 @@ export const info = {
       logo: CarletonLogo,
       title: "Teaching Assistant",
       date: "09/2020 - Present",
-      description: "Participating in engaging tutorial sections to help professors guide more than 1000 students of three semesters through materials of Computer Science courses. Holding weekly office hours to answer questions and help students improve their understanding of the materials. Grading and providing detailed feedback for students’ assignments and tests."
+      description: "Assisting professors to guide students through materials of Computer Science courses. Marking assignments and giving students thorough feedback."
     },
     {
       company: "Kinaxis",
       logo: KinaxisLogo,
       title: "Software Developer Intern",
       date: "09/2021 - 12/2021",
-      description: "Developed an interactive and responsive data visualization with D3.js library integrated in JavaScript + HTML environment. Improved the ease to analyze allotments data for supply planners and increased debugging productivity in the related field. At the same time, it helped onboard Kinaxis’s new developers by providing the visual result of internal allotment algorithms."
+      description: "Developed an interactive and responsive data visualization with D3.js library integrated with TypeScript and HTML, that improves development and onboarding procedures at the company."
     }, 
     {
       company: "Kinaxis",
       logo: KinaxisLogo,
       title: "Software Engineering Intern",
       date: "05/2021 - 08/2021",
-      description: "Investigated and analyzed a specialized product structure cycle detection graph algorithm that combines variances of strongly connected components and cycles enumeration algorithms. Implemented the algorithm using the company’s internal C++ frameworks to solve lasting problems in the old algorithm that create cycle duplications in the result to enhance the supply planning process for over 100 global enterprises. Improved the running time of the platform in cycles detection from more than 12 hours to 1 second on a customer dataset and produced quality result data models without existing issues from the old cycle detector. Validated the correctness of the algorithm on real data set through unit tests that exceed the 75% coverage goal."
+      description: "Investigated and implemented in C++ a specialized product structure cycle detection graph algorithm that combines variances of strongly connected components and cycles enumeration algorithms. Enhanced the running time and output reports of Kinaxis's related analytics algorithm."
     },
     {
       company: "CU Blueprint",
       logo: BlueprintLogo,
       title: "Volunteer Front-end Developer",
       date: "09/2020 - 08/2021",
-      description: "Developed a CRM full-stack web application for a non-profit organization that significantly improves the processing time of their services by migrating to software-automatic workflow. Coordinated with developers and designers in an Agile team to build a user-friendly and responsive user interface with reusable components using React, CSS and Material UI that integrates with Node.js server."
+      description: "Using React, coordinated with backend developers and designers to build a CRM full-stack web app for a non-profit organization that significantly improves the processing time of their services."
     },
     {
       company: "Carleton University",
       logo: CarletonLogo,
       title: "Undergraduate Researcher",
       date: "01/2021 - 04/2021",
-      description: "Studied Finger Search, an extension that improves the average running time of operations that require searching. Then, applied the operation on Treap, SkipList and 2-4 Tree using C++ following up by examining and reporting their differences in time complexity to decide which data structure is the most suitable."
+      description: "Studied Finger Search, an extension that improves the average running time of search operation. Then, applied in C++ and tested the algorithm on different data structures."
     },
     {
       company: "Carleton University",
       logo: CarletonLogo,
       title: "Undergraduate Researcher",
       date: "05/2020 - 05/2020",
-      description: "Explored a divide-and-conquer algorithm that calculates the closest-pair distance of points on multi-dimensional spaces without knowing coordinates using the doubling dimension definition and implemented it from scratch with C++ and Boost library. Proved the algorithm’s logarithmic running time in practice by analyzing the output data and successfully led the original research project to a conclusion with the confirmation of the involving professors."
+      description: "Explored a divide-and-conquer algorithm that calculates the closest-pair distance of points on multi-dimensional spaces without knowing their coordinates, using the doubling dimension definition. Implemented the algorithm from scratch with C++ and Boost library, then proved its logarithmic running time hypothesis."
     },
   ]
 }
