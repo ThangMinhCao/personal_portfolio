f510488d80b7a564d89ce648af2563be3892667b
ThangMinhCao <thangminhcao@gmail.com>
2022-01-27 20:11:08 -0500

    Modify grid layout of project page

--- src/components/Section.js
+++ src/components/Section.js
@@ -4,7 +4,7 @@ const Section = styled.section`
   ${'' /* background: ${(props) => props.backgroundColor}; */}
   min-height: 100vh;
   scroll-snap-align: start;
-  min-width: fit-content;
+  min-width: 300px;
 `
 
 export default Section;
