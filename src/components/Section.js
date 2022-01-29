20ccc923af9315c936a86be937d26e2399a14dc2
ThangMinhCao <thangminhcao@gmail.com>
2022-01-29 00:26:55 -0500

    Add mibile webkit 100vh fix

--- src/components/Section.js
+++ src/components/Section.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 const Section = styled.section`
   min-height: 100vh;
+  min-height: -webkit-fill-available;
   scroll-snap-align: start;
   min-width: 300px;
 `
