32b3b5101522ecba16e11a6a2c43dcfd28279e47
Thang Cao <37521929+ThangMinhCao@users.noreply.github.com>
2022-02-13 21:05:12 -0500

    Update Section.js

--- src/components/Section.js
+++ src/components/Section.js
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 const Section = styled.section`
   min-height: 100vh;
-//   min-height: -webkit-fill-available;
   scroll-snap-align: start;
   min-width: 300px;
 `
