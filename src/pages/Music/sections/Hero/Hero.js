95079afdef103cbc94738d316d2a59a5a6a74528
ThangMinhCao <thangminhcao@gmail.com>
2024-01-09 15:02:08 -0500

    Adjust zooming rate

--- src/pages/Music/sections/Hero/Hero.js
+++ src/pages/Music/sections/Hero/Hero.js
@@ -15,14 +15,14 @@ function MusicHero() {
     offset: ["0 0", "2.2 0"],
   });
   const zoom = useTransform(scrollYProgress, [0, 0.15, 1], [1, 1, 3.5]);
-  const zoom2 = useTransform(scrollYProgress, [0, 0.15, 1], [0.1, 0.1, 5.5]);
+  const zoom2 = useTransform(scrollYProgress, [0, 0.15, 1], [0.01, 0.05, 5.5]);
   const opacity = useTransform(scrollYProgress, [0, 0.83, 1], [0, 0, 5]);
   const opacity2 = useTransform(
     scrollYProgress,
     [0, 0.15, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1],
     [0.05, 0.05, 0.4, 0.7, 0.9, 1, 0.9, 1, 0.9, 1, 1]
   );
-  const opacity3 = useTransform(scrollYProgress, [0, 0.1], [0, 1]);
+  const opacity3 = useTransform(scrollYProgress, [0, 0.01, 0.05], [0, 0.5, 1]);
 
   return (
     <MusicSection key="music-hero" className="music-hero-container">
